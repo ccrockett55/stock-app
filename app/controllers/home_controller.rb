4b958f65c65e789e53454cdd1b454a5081c0d37e
Li Crockett <ccrockett55@gmail.com>
2021-11-29 11:30:49 -0600

    installed Devise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,18 @@
 class HomeController < ApplicationController
   def index
+    @api = StockQuote::Stock.new(api_key: "pk_15feaedd30a54b698648109bf4e95fe4")
+
+    if  params[:ticker] == ""
+      @nothing = 'Please enter a ticker symbol'
+    elsif params[:ticker]
+        @stock = StockQuote::Stock.quote(params[:ticker])
+        if !@stock
+          @error = "Wrong Symbol!"
+        end
+
+  end
+end
+
+  def about
   end
 end
